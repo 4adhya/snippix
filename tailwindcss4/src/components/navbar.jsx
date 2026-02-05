4944cca924dcc276443c5f2a561a4e5901a68ebd
Janvimathur2006 <mathurj9900@gmail.com>
2026-02-05 21:17:49 +0530

    bell icon removed

--- tailwindcss4/src/components/navbar.jsx
+++ tailwindcss4/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Bell, User, Settings, Search } from "lucide-react";
+import { User, Settings, Search } from "lucide-react";
 
 export default function Navbar({ onSearchClick }) {
   const navigate = useNavigate();
@@ -27,8 +27,12 @@ export default function Navbar({ onSearchClick }) {
           onClick={onSearchClick}
         />
 
-        <Bell size={22} className="cursor-pointer opacity-80 hover:opacity-100" />
-        <User size={22} className="cursor-pointer opacity-80 hover:opacity-100" />
+        <User
+          size={22}
+          className="cursor-pointer opacity-80 hover:opacity-100"
+          onClick={() => navigate("/")}
+        />
+
         <Settings
           size={22}
           className="cursor-pointer opacity-80 hover:opacity-100"
